a8e7bf1053c2f3433e1a9d90e555325126b4da4c
ZiberEvhenii <ziberz@mail.ru>
2015-12-17 20:27:00 +0200

    Add Player

---
+++ src/Player.java
@@ -0,0 +1,18 @@
+
+public class Player {
+    public void load(){
+
+    }
+    void play(){
+
+    }
+    public void stop(){
+
+    }
+    public void pause(){
+
+    }
+    public void resume(){
+
+    }
+}
