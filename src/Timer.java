698d726e583397e210b4686ba3043029eede8b52
ZiberEvhenii <ziberz@mail.ru>
2015-12-23 21:42:56 +0200

    add class Timer

---
+++ src/Timer.java
@@ -0,0 +1,73 @@
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Timer extends Thread{
+    private DateFormat dateFormater = new SimpleDateFormat("HH:mm:ss");
+    private boolean isRunning = false;
+    private boolean isPause = false;
+    private boolean isReset = false;
+    private long startTime;
+    private long pauseTime;
+
+    private JLabel labelRecordTime;
+    private JSlider slider;
+    private Clip audioClip;
+
+    public void setAudioClip(Clip audioClip) {
+        this.audioClip = audioClip;
+    }
+    Timer(JLabel labelRecordTime, JSlider slider) {
+        this.labelRecordTime = labelRecordTime;
+        this.slider = slider;
+    }
+    public void run() {
+        isRunning = true;
+
+        startTime = System.currentTimeMillis();
+
+        while (isRunning) {
+
+
+            if (!isPause) {
+                if (audioClip != null && audioClip.isRunning()) {
+                    labelRecordTime.setText(toTimeString());
+                    int currentSecond = (int) audioClip.getMicrosecondPosition() / 1000000;
+                    slider.setValue(currentSecond);
+                }
+            } else {
+                pauseTime += 100;
+            }
+
+
+            if (isReset) {
+                slider.setValue(0);
+                labelRecordTime.setText("00:00:00");
+                isRunning = false;
+                break;
+            }
+        }
+    }
+
+    void reset() {
+        isReset = true;
+        isRunning = false;
+    }
+    void pauseTimer() {
+        isPause = true;
+    }
+    void resumeTimer() {
+        isPause = false;
+    }
+
+    private String toTimeString() {
+        long now = System.currentTimeMillis();
+        Date current = new Date(now - startTime - pauseTime);
+        dateFormater.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String timeCounter = dateFormater.format(current);
+        return timeCounter;
+    }
+}
