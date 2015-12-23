c531fe7cfb64d06ab5202c84bed60a5d3898306f
ZiberEvhenii <ziberz@mail.ru>
2015-12-23 21:42:42 +0200

    add class Music

---
+++ src/Music.java
@@ -0,0 +1,21 @@
+
+public interface Music {
+    String[] items = {
+            "AC.DC - Rock or Bust.wav",
+            "Avicii - Feeling Good.wav",
+            "Justin Bieber - What Do You Mean.wav",
+            "Marina And The Diamonds - Electra Heart.wav",
+            "Troye Sivan - BITE.wav",
+            "Kwabs - Walk.wav"
+    };
+
+    String[] items1 = {
+            "Apple-Music.jpeg.pagespeed.ce.L1OdXzzU7J.jpg",
+            "Avicii - Feeling Good.png",
+            "Justin Bieber - What Do You Mean.jpg",
+            "Marina And The Diamonds - Electra Heart.jpg",
+            "Troye Sivan - BITE.png",
+            "Kwabs - Walk.jpg"
+    };
+
+}
