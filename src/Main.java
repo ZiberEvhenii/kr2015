24cda32aff0121ca51ef8564a2d7ec0412c51d83
ZiberEvhenii <ziberz@mail.ru>
2015-12-23 20:41:38 +0200

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        //new Form();
+        SwingUtilities.invokeLater(new Runnable() {
+
+
+            public void run() {
+                new Frame();
+            }
+        });
 
 
     }
